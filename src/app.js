46337cd4549597eb0f54c62ccb6d80c60b35362e
bikashpdyadav <bikashpdyadav11@gmail.com>
2024-12-21 13:33:33 +0530

    Tested GET, POST and DELETE api using POSTMAN

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.use("/",(req,res) => {
+app.get("/user",(req,res) => {
+    res.send({firstName:"Bikash", lastName:"Yadav"});
+})
+app.use("/test",(req,res) => {
     res.send("Namaste Bikash");
 })
-
-app.use("/contact",(req,res) => {
-    res.send("Namaste Bikash @Contact");
+app.post("/user",(req,res) => {
+    res.send("Data saved success.")
 })
-
-app.listen(7777, () => {
+app.delete("/user",(req,res) => {
+    res.send("Data deleted success.")
+})
+app.listen(3000, () => {
     console.log("Server is running...");
 })
 console.log("!Shree Ganesha!")
