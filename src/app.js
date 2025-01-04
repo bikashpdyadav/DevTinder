d31c5e9ac83ab50505a8f1f8e547bb138888b75a
bikashpdyadav <bikashpdyadav11@gmail.com>
2025-01-04 20:14:44 +0530

    Validations in API level

--- src/app.js
+++ src/app.js
@@ -13,6 +13,28 @@ app.post('/signup', async (req,res) => {
     res.send("User added successfully!!");
 });
 
+app.patch('/user/:userId', async (req,res) => {
+    const userId = req.params?.userId;
+    const data = req.body;
+    try{
+        const ALLOWED_UPDATES = [
+            "userId", "photoUrl", "about", "gender", "age", "skills"
+        ]
+    
+        const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
+    
+        if(!isUpdateAllowed){
+            throw new Error("Update Failed");
+        }
+        const user = await User.findByIdAndUpdate({ _id: userId}, data, {
+            returnDocument: "after",
+            runValidators: true,
+        })
+    }catch(err){
+        res.status(400).send("Update failed: "+err.message);
+    }
+})
+
 app.get('/user', async (req,res) => {
     const email = req.body.emailId;
     try{
